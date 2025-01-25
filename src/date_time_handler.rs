0f5794c3cd5afeb223b46425d32b41e023443a1a
Faisal-9999 <mukhtarfaisal03@gmail.com>
2025-01-25 23:13:12 +0500

    Date Time Handler feature underway

--- src/date_time_handler.rs
+++ src/date_time_handler.rs
@@ -19,4 +19,49 @@ impl DateTimehandler {
         }
         
     }
+
+    fn show(&mut self, ui : &mut Ui) -> Option<(NaiveDate, u32, u32)> {
+        let mut is_confirmed = false;
+
+        ui.label("Select Date: ");
+        ui.horizontal(|ui| {
+            if ui.button("Previous").clicked() {
+                self.selected_date = self.selected_date.pred_opt().unwrap_or(self.selected_date);
+            }
+
+            ui.label(self.selected_date.to_string());
+
+            if ui.button("Next").clicked() {
+                self.selected_date = self.selected_date.succ_opt().unwrap_or(self.selected_date);
+            }
+        });
+
+        ui.label("Select Time: ");
+        ui.horizontal(|ui| {
+            ui.label("Hour: ")
+            if ui.add(DragValue::new(&mut self.selected_hour).range(0..=23)).changed() {
+                self.selected_hour = self.selected_hour % 24;
+            }
+
+            ui.label("Minute: ");
+            if ui.add(DragValue::new(&mut self.selected_minute).range(0..=59)).changed() {
+                self.selected_minute = self.selected_minute % 60;
+            }
+        });
+
+        if ui.button("Set").clicked() {
+            is_confirmed = true;
+        }
+
+        if is_confirmed {
+            Some((
+                self.selected_date,
+                self.selected_hour,
+                self.selected_minute,
+            ))
+        }
+        else {
+            None
+        }
+    }
 }
